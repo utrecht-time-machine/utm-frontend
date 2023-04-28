a05e05a97562fd1b504b0d096ff59d09c4561b1b
Rick Companje <rick@companje.nl>
2023-04-28 13:17:09 +0200

    update api all_locations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   apiUrl: 'https://test.utrechttimemachine.nl/rest/',
   imageBaseUrl: 'https://test.utrechttimemachine.nl/',
   apiSuffixes: {
-    mapLocations: 'location',
+    mapLocations: 'all_locations',
   },
 };
