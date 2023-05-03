5d6a3926fe481f89bc537f0259c3b77c33d90b9e
Simon <Simon-Dirks@users.noreply.github.com>
2023-05-03 15:04:34 +0200

    Update route page

--- src/app/components/routes/routes.component.ts
+++ src/app/components/routes/routes.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-routes',
   templateUrl: './routes.component.html',
-  styleUrls: ['./routes.component.scss']
+  styleUrls: ['./routes.component.scss'],
 })
 export class RoutesComponent {
-
+  constructor(public router: Router) {}
 }
