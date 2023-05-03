986596db6311ddd1e47054878dfa76f6409d6d60
Simon <Simon-Dirks@users.noreply.github.com>
2023-05-03 16:08:38 +0200

    Add placeholder content and layout for routes page

--- src/app/components/map/map.module.ts
+++ src/app/components/map/map.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MapComponent } from './map.component';
 import { SelectedItemComponent } from './selected-item/selected-item.component';
 import { RouterModule } from '@angular/router';
+import { SelectedRouteComponent } from './selected-route/selected-route.component';
+import { RouteStepsComponent } from './route-steps/route-steps.component';
 
 @NgModule({
-  declarations: [MapComponent, SelectedItemComponent],
+  declarations: [MapComponent, SelectedItemComponent, SelectedRouteComponent, RouteStepsComponent],
   exports: [MapComponent],
   imports: [CommonModule, RouterModule],
 })
---
+++ src/app/components/map/route-steps/route-steps.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-route-steps',
+  templateUrl: './route-steps.component.html',
+  styleUrls: ['./route-steps.component.scss']
+})
+export class RouteStepsComponent {
+
+}
