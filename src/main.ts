80717991c74643d3711e635269836b2f8411aa83
Simon <Simon-Dirks@users.noreply.github.com>
2023-05-24 12:23:10 +0200

    Add Cordova config

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,21 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+const bootstrap = () => {
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
+};
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const isCordovaApp = typeof (window as any)['cordova'] !== 'undefined';
+if (isCordovaApp) {
+  document.addEventListener(
+    'deviceready',
+    () => {
+      bootstrap();
+    },
+    false
+  );
+} else {
+  bootstrap();
+}
