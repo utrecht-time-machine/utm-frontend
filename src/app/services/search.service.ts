9f72e9cb55c3e0fc5e27974ea73f2931e3c6199d
Simon <Simon-Dirks@users.noreply.github.com>
2023-05-31 10:20:27 +0200

    Convert autocomplete search html format

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -27,10 +27,33 @@ export class SearchService {
     const searchResults: LiveSearchResult[] = await lastValueFrom(
       this.http.get<LiveSearchResult[]>(environment.liveSearchUrl + searchInput)
     );
+
+    for (const searchResult of searchResults) {
+      searchResult.label = this._convertSearchResultLabelHtmlFormat(
+        searchResult.label
+      );
+    }
+
     setTimeout(() => {
       this.liveSearchResults.next(searchResults);
       this.isLoadingLiveSearchResults = false;
     }, 500);
+  }
+
+  private _convertSearchResultLabelHtmlFormat(labelHtml: string): string {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(labelHtml, 'text/html');
+
+    const nameElement = doc.querySelector('.field--name-title');
+    if (nameElement) {
+      const name = nameElement?.textContent || '';
+
+      // TODO: Add address data if/when available
+      const updatedHtml = `<div class="search-view"><div class="name"><span>${name}</span></div><div class="addr"></div></div>`;
+      return updatedHtml;
+    }
+
+    return `<div class="search-view"><div class="name"><span>${labelHtml}</span></div><div class="addr"></div></div>`;
   }
 
   removeLiveSearchResults() {
