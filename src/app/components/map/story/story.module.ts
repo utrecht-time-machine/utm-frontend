06ac1280b307b0082e43ac8fa9963a69fe138cf6
Simon <Simon-Dirks@users.noreply.github.com>
2023-05-31 16:04:05 +0200

    Add translation value

--- src/app/components/map/story/story.module.ts
+++ src/app/components/map/story/story.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoryComponent } from './story.component';
 import { MediaItemModule } from '../media-item/media-item.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [StoryComponent],
   exports: [StoryComponent],
-  imports: [CommonModule, MediaItemModule],
+  imports: [CommonModule, MediaItemModule, TranslateModule],
 })
 export class StoryModule {}
