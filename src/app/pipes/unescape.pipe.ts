ba5376133854545d6d775718ac5b48b9ef2b93ed
Simon <Simon-Dirks@users.noreply.github.com>
2023-06-21 14:22:42 +0200

    Add TODO

--- src/app/pipes/unescape.pipe.ts
+++ src/app/pipes/unescape.pipe.ts
@@ -5,6 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class UnescapePipe implements PipeTransform {
   transform(value: any, args?: any): any {
+    // TODO: Do not use in SSR-mode
     const doc = new DOMParser().parseFromString(value, 'text/html');
     return doc.documentElement.textContent;
   }
