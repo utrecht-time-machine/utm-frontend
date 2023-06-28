f3877a14cbfdaf40045fd0976abf83c06460eebb
Simon <Simon-Dirks@users.noreply.github.com>
2023-06-28 16:25:16 +0200

    Show organisations for media items

--- src/app/components/map/media-item/media-item.module.ts
+++ src/app/components/map/media-item/media-item.module.ts
@@ -4,10 +4,17 @@ import { MediaItemComponent } from './media-item.component';
 import { SafeModule } from '../../../pipes/safe.module';
 import { LicenseModule } from '../../license/license.module';
 import { TranslateModule } from '@ngx-translate/core';
+import { OrganisationsModule } from '../../organisations/organisations.module';
 
 @NgModule({
   declarations: [MediaItemComponent],
-  imports: [CommonModule, SafeModule, LicenseModule, TranslateModule],
+  imports: [
+    CommonModule,
+    SafeModule,
+    LicenseModule,
+    TranslateModule,
+    OrganisationsModule,
+  ],
   exports: [MediaItemComponent],
 })
 export class MediaItemModule {}
