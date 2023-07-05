8b6b3a2400140bc66a6d0ac67c3a016b716a06e3
Simon <Simon-Dirks@users.noreply.github.com>
2023-07-05 15:57:22 +0200

    Show edit button using localstorage key

--- src/app/components/edit-button/edit-button.component.ts
+++ src/app/components/edit-button/edit-button.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { PlatformService } from '../../services/platform.service';
 
 @Component({
   selector: 'app-edit-button',
@@ -10,4 +11,17 @@ export class EditButtonComponent {
   @Input() nid: string | undefined;
 
   environment = environment;
+
+  constructor(private platform: PlatformService) {}
+
+  shouldBeShown(): boolean {
+    if (this.platform.isBrowser()) {
+      return (
+        environment.dev ||
+        window.localStorage.getItem('utrechtTimeMachineDev') === 'true'
+      );
+    }
+
+    return environment.dev;
+  }
 }
