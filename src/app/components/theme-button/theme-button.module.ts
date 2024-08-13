8fb709c69535e4ddb1b7583c3e2baa14087dad08
Simon <Simon-Dirks@users.noreply.github.com>
2024-08-13 15:10:14 +0200

    Translate theme button

--- src/app/components/theme-button/theme-button.module.ts
+++ src/app/components/theme-button/theme-button.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemeButtonComponent } from './theme-button.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [ThemeButtonComponent],
   exports: [ThemeButtonComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
 })
 export class ThemeButtonModule {}
