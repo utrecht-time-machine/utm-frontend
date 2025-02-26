5fb2cf4ac42218e9f6bd5a404a99ecf138b8e131
Simon <mail@simondirks.com>
2025-02-26 10:55:55 +0100

    Add time and theme headers

---
+++ src/app/components/filters/filter/filter-header/filter-header.component.ts
@@ -0,0 +1,16 @@
+import { NgIf } from '@angular/common';
+import { Component, Input, type OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-header',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './filter-header.component.html',
+  styleUrls: ['./filter-header.component.scss'],
+})
+export class FilterHeaderComponent implements OnInit {
+  @Input() title: string = '';
+  @Input() icon: string = '';
+
+  ngOnInit(): void {}
+}
--- src/app/components/filters/filter/filter.module.ts
+++ src/app/components/filters/filter/filter.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FilterComponent } from './filter.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatIconModule } from '@angular/material/icon';
+import { FilterHeaderComponent } from "./filter-header/filter-header.component";
 
 @NgModule({
   declarations: [FilterComponent],
-  imports: [CommonModule, TranslateModule, MatIconModule],
+  imports: [CommonModule, TranslateModule, MatIconModule, FilterHeaderComponent],
   exports: [FilterComponent],
 })
 export class FilterModule {}
