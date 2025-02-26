7d95abdb242fada174ab577a7dd1427db3b045b7
Simon <mail@simondirks.com>
2025-02-26 10:38:12 +0100

    Add filter panel header

---
+++ src/app/components/filters/filters-header/filters-header.component.ts
@@ -0,0 +1,12 @@
+import { Component, type OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filters-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './filters-header.component.html',
+  styleUrls: ['./filters-header.component.scss'],
+})
+export class FiltersHeaderComponent implements OnInit {
+  ngOnInit(): void {}
+}
--- src/app/components/filters/filters.module.ts
+++ src/app/components/filters/filters.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FiltersComponent } from './filters.component';
 import { ThemeFilterModule } from './filter/theme-filter/theme-filter.module';
 import { TimeFilterModule } from './filter/time-filter/time-filter.module';
+import { FiltersHeaderComponent } from "./filters-header/filters-header.component";
 
 @NgModule({
   declarations: [FiltersComponent],
-  imports: [CommonModule, ThemeFilterModule, TimeFilterModule],
+  imports: [CommonModule, ThemeFilterModule, TimeFilterModule, FiltersHeaderComponent],
   exports: [FiltersComponent],
 })
 export class FiltersModule {}
