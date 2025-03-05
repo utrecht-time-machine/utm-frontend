69548fefc38ba579187c9a07abd916e463a11088
Simon <mail@simondirks.com>
2025-03-05 15:12:18 +0100

    Bring back 3D buildings layer

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -156,6 +156,51 @@ export class MapService {
     this.initRefreshOnTimeChange();
   }
 
+  add3DBuildingsLayer() {
+    const layers = this.map?.getStyle().layers;
+    const labelLayerId = layers?.find(
+      (layer: any) => layer.type === 'symbol' && layer.layout['text-field']
+    )?.id;
+
+    this.map?.addLayer(
+      {
+        id: 'add-3d-buildings',
+        source: 'composite',
+        'source-layer': 'building',
+        filter: ['==', 'extrude', 'true'],
+        type: 'fill-extrusion',
+        minzoom: 12,
+        paint: {
+          'fill-extrusion-color': '#aaa',
+
+          // Use an 'interpolate' expression to
+          // add a smooth transition effect to
+          // the buildings as the user zooms in.
+          'fill-extrusion-height': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            15,
+            0,
+            15.05,
+            ['get', 'height'],
+          ],
+          'fill-extrusion-base': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            15,
+            0,
+            15.05,
+            ['get', 'min_height'],
+          ],
+          'fill-extrusion-opacity': 0.6,
+        },
+      },
+      labelLayerId
+    );
+  }
+
   initMap() {
     // setTimeout(() => {
     //   this.showSpinner = true;
@@ -219,6 +264,8 @@ export class MapService {
     });
 
     this.map.on('style.load', () => {
+      this.add3DBuildingsLayer();
+
       if (this.routing.getSelectedView() === SelectedView.Locations) {
         this.addLocationsOnMap(false);
       } else if (
