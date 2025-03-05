47a4fba142489ee3bb392a1a2319e1e62b244596
Simon <mail@simondirks.com>
2025-03-05 12:24:04 +0100

    Animate active filter badge

--- src/app/components/filters/active-filters-badge/active-filters-badge.component.ts
+++ src/app/components/filters/active-filters-badge/active-filters-badge.component.ts
@@ -1,4 +1,5 @@
 import { Component, type OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 import { ThemeService } from 'src/app/services/theme.service';
 import { TimeService } from 'src/app/services/time.service';
 
@@ -6,6 +7,17 @@ import { TimeService } from 'src/app/services/time.service';
   selector: 'app-active-filters-badge',
   templateUrl: './active-filters-badge.component.html',
   styleUrls: ['./active-filters-badge.component.scss'],
+  animations: [
+    trigger('fadeSlide', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translate(50%, -30%)' }),
+        animate('200ms ease-out', style({ opacity: 1, transform: 'translate(50%, -50%)' }))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({ opacity: 0, transform: 'translate(50%, -70%)' }))
+      ])
+    ])
+  ]
 })
 export class ActiveFiltersBadgeComponent implements OnInit {
   constructor(public theme: ThemeService, public time: TimeService) {}
--- src/app/components/filters/filter/filter-header/filter-header.component.ts
+++ src/app/components/filters/filter/filter-header/filter-header.component.ts
@@ -1,5 +1,6 @@
 import { NgIf } from '@angular/common';
 import { Component, Input, type OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-filter-header',
@@ -7,6 +8,23 @@ import { Component, Input, type OnInit } from '@angular/core';
   imports: [NgIf],
   templateUrl: './filter-header.component.html',
   styleUrls: ['./filter-header.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateX(-5px)' }),
+        animate(
+          '150ms ease-out',
+          style({ opacity: 1, transform: 'translateX(0)' })
+        ),
+      ]),
+      transition(':leave', [
+        animate(
+          '150ms ease-in',
+          style({ opacity: 0, transform: 'translateX(-5px)' })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class FilterHeaderComponent implements OnInit {
   @Input() title: string = '';
