3006a005bc9e1ca08b334155a26415b84b189ccc
Simon <mail@simondirks.com>
2025-03-18 12:20:50 +0100

    Add comment sending status texts and spinner

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 import { FeedbackService } from '../../services/feedback.service';
 import { FeedbackRating } from '../../models/feedback-rating';
 
@@ -6,6 +7,15 @@ import { FeedbackRating } from '../../models/feedback-rating';
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
   styleUrls: ['./feedback.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('200ms ease-out', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [animate('200ms ease-in', style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class FeedbackComponent {
   @Input() feedbackItemId: string | undefined;
@@ -14,7 +24,12 @@ export class FeedbackComponent {
 
   FeedbackRating = FeedbackRating;
 
-  feedbackText: string = '';
+  commentText: string = '';
+  isSubmittingComment = false;
+  commentSubmitStatus: 'none' | 'success' | 'error' = 'none';
+  commentSubmitMessage = '';
+  private commentSubmitStatusTimeout: number | null = null;
+  private commentStatusMessageHideAfterMs: number = 1500;
 
   constructor(public feedbackService: FeedbackService) {}
 
@@ -34,5 +49,38 @@ export class FeedbackComponent {
     return (
       this.feedbackService.getItemRating(this.feedbackItemId) !== undefined
     );
+  }
+
+  private showStatusMessage(status: 'success' | 'error', message: string) {
+    this.commentSubmitStatus = status;
+    this.commentSubmitMessage = message;
+
+    if (this.commentSubmitStatusTimeout !== null) {
+      window.clearTimeout(this.commentSubmitStatusTimeout);
+    }
+
+    this.commentSubmitStatusTimeout = window.setTimeout(() => {
+      if (this.commentSubmitStatus !== 'none') {
+        this.commentSubmitStatus = 'none';
+        this.commentSubmitMessage = '';
+      }
+      this.commentSubmitStatusTimeout = null;
+    }, this.commentStatusMessageHideAfterMs);
+  }
+
+  async submitComment() {
+    if (this.isSubmittingComment || !this.feedbackItemId) return;
+
+    this.isSubmittingComment = true;
+
+    try {
+      await this.feedbackService.comment(this.feedbackItemId, this.commentText);
+      this.showStatusMessage('success', 'Feedback verstuurd');
+      this.commentText = '';
+    } catch (error) {
+      this.showStatusMessage('error', 'Er ging iets mis');
+    }
+
+    this.isSubmittingComment = false;
   }
 }
