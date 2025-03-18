6dab9e3ee78530d47016aff26768ebca44b0a994
Simon <mail@simondirks.com>
2025-03-18 15:18:13 +0100

    Scroll to top of transcription text when it updates

--- src/app/components/audio-transcript/audio-transcript.component.ts
+++ src/app/components/audio-transcript/audio-transcript.component.ts
@@ -1,5 +1,12 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component, Input, type OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -10,11 +17,26 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './audio-transcript.component.html',
   styleUrls: ['./audio-transcript.component.css'],
 })
-export class AudioTranscriptComponent implements OnInit {
+export class AudioTranscriptComponent implements OnChanges {
   @Input() transcript?: string;
   isVisible = false;
 
-  ngOnInit(): void {}
+  @ViewChild('transcriptContent')
+  transcriptContent?: ElementRef<HTMLDivElement>;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['transcript'] && !changes['transcript'].firstChange) {
+      setTimeout(() => {
+        this._scrollToTop();
+      });
+    }
+  }
+
+  private _scrollToTop(): void {
+    if (this.transcriptContent) {
+      this.transcriptContent.nativeElement.scrollTop = 0;
+    }
+  }
 
   getTranscriptLines(): string[] {
     return this.transcript?.split('\n') ?? [];
