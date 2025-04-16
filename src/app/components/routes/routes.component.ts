d1cd3b643387a3dbddcfc5b5964ba2a7643cf83e
Simon Dirks <mail@simondirks.com>
2025-04-16 15:33:03 +0200

    Fade out routes that do not match filters

--- src/app/components/routes/routes.component.ts
+++ src/app/components/routes/routes.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UtmRoutesService } from '../../services/utm-routes.service';
 import { UtmTranslateService } from '../../services/utm-translate.service';
+import { ThemeService } from 'src/app/services/theme.service';
+import { TimeService } from 'src/app/services/time.service';
+import { UtmRoute } from 'src/app/models/utm-route';
 
 @Component({
   selector: 'app-routes',
@@ -12,8 +15,21 @@ export class RoutesComponent {
   constructor(
     public router: Router,
     public utmRoutes: UtmRoutesService,
-    public utmTranslate: UtmTranslateService
+    public utmTranslate: UtmTranslateService,
+    private themes: ThemeService,
+    private time: TimeService
   ) {}
+
+  isRouteVisible(route: UtmRoute): boolean {
+    const passesThemeFilter = this.themes.shouldShow(route.theme_ids || []);
+
+    const passesTimeFilter = this.time.isInSelectedRange(
+      [route.min_date_str || ''],
+      [route.max_date_str || '']
+    );
+
+    return passesThemeFilter && passesTimeFilter;
+  }
 
   async selectRoute(url: string, nid: string) {
     await this.utmRoutes.selectByUrlOrId(url, nid);
