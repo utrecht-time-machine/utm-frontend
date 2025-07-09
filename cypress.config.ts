1703f59d96552019251ab0171719d985b1192a28
Simon Dirks <mail@simondirks.com>
2025-07-09 13:04:54 +0200

    Run e2e before deploy

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: process.env['CYPRESS_BASE_URL'] || 'http://localhost:4200',
+    supportFile: 'cypress/support/e2e.ts',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    video: false,
+    screenshotOnRunFailure: false,
+  },
+});
