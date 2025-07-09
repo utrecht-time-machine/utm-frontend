804ab7cd6c10dbb516f0f3809022d4ddd436150a
Simon Dirks <mail@simondirks.com>
2025-07-09 14:17:26 +0200

    Disable feedback test

--- cypress/e2e/feedback.cy.ts
+++ cypress/e2e/feedback.cy.ts
@@ -1,21 +1,19 @@
 import { environment } from '../support/environment-utils';
 
 describe('Feedback', () => {
-  it('should submit feedback', () => {
-    cy.intercept('POST', environment.commentPostUrl, (req) => {
-      expect(req.body).to.deep.equal({
-        itemId: '1069',
-        comment: 'Automated test',
-      });
-
-      req.continue();
-    }).as('submitFeedback');
-
-    cy.visit('/locaties/domtoren?story=de-domtoren-een-waanzinnig-bouwproject');
-    cy.get('app-story app-feedback textarea').type('Automated test');
-    cy.get('app-story app-feedback button').click();
-
-    cy.wait('@submitFeedback').its('response.statusCode').should('eq', 200);
-    cy.contains('Bedankt voor je feedback').should('be.visible');
-  });
+  // TODO: Bring back feedback test
+  // it('should submit feedback', () => {
+  //   cy.intercept('POST', environment.commentPostUrl, (req) => {
+  //     expect(req.body).to.deep.equal({
+  //       itemId: '1069',
+  //       comment: 'Automated test',
+  //     });
+  //     req.continue();
+  //   }).as('submitFeedback');
+  //   cy.visit('/locaties/domtoren?story=de-domtoren-een-waanzinnig-bouwproject');
+  //   cy.get('app-story app-feedback textarea').type('Automated test');
+  //   cy.get('app-story app-feedback button').click();
+  //   cy.wait('@submitFeedback').its('response.statusCode').should('eq', 200);
+  //   cy.contains('Bedankt voor je feedback').should('be.visible');
+  // });
 });
