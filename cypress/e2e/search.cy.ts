497e930115504cf70ed424d29221037732f8dbfc
Simon Dirks <mail@simondirks.com>
2025-07-09 11:38:19 +0200

    Create search.cy.ts

---
+++ cypress/e2e/search.cy.ts
@@ -0,0 +1,17 @@
+describe('Search', () => {
+  it('should show live results for search field', () => {
+    cy.visit('/locaties');
+    cy.get('input#edit-livesearch').click().type('ACU');
+
+    cy.get('.search-api-autocomplete-search').should('exist');
+
+    cy.get('.search-api-autocomplete-search li').should(
+      'have.length.greaterThan',
+      1
+    );
+
+    cy.get('.search-api-autocomplete-search li')
+      .contains('ACU')
+      .should('exist');
+  });
+});
