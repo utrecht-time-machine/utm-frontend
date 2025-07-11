cd60fb8f0faf2847d55ed58471bccf56ff5d6b74
Simon Dirks <mail@simondirks.com>
2025-07-11 12:55:40 +0200

    Update feedback.cy.ts

--- cypress/e2e/feedback.cy.ts
+++ cypress/e2e/feedback.cy.ts
@@ -1,7 +1,7 @@
 import { environment } from '../support/environment-utils';
 
 describe('Feedback', () => {
-  it('should submit feedback', () => {
+  it('should submit feedback for story', () => {
     cy.intercept('POST', environment.commentPostUrl, (req) => {
       expect(req.body).to.deep.equal({
         itemId: '1069',
@@ -12,6 +12,23 @@ describe('Feedback', () => {
     cy.visit('/locaties/domtoren?story=de-domtoren-een-waanzinnig-bouwproject');
     cy.get('app-story app-feedback textarea').type('Automated test');
     cy.get('app-story app-feedback button').click();
+    cy.wait('@submitFeedback').its('response.statusCode').should('eq', 200);
+    cy.contains('Bedankt voor je feedback').should('be.visible');
+  });
+
+  it('should submit feedback for location', () => {
+    cy.intercept('POST', environment.commentPostUrl, (req) => {
+      expect(req.body).to.deep.equal({
+        itemId: '598',
+        comment: 'Automated test for location',
+      });
+      req.continue();
+    }).as('submitFeedback');
+    cy.visit('/locaties/domtoren');
+    cy.get('app-selected-item app-feedback textarea').type(
+      'Automated test for location'
+    );
+    cy.get('app-selected-item app-feedback button').click();
     cy.wait('@submitFeedback').its('response.statusCode').should('eq', 200);
     cy.contains('Bedankt voor je feedback').should('be.visible');
   });
