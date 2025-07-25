e9e1f46639a27b256c40f0e426fd64672a401f9d
Simon Dirks <mail@simondirks.com>
2025-07-25 12:51:02 +0200

    Show original image, even if source and license are not set

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -7,9 +7,9 @@ export class ImageService {
   constructor() {}
 
   getImageUrl(originalUrl: string, source?: string, license?: string): string {
-    if (!originalUrl || !source || !license) {
-      return 'https://data.utrechttimemachine.nl/sites/default/files/2025-05/Afbeelding-tijdelijk-niet-beschikbaar_3.png';
-    }
+    // if (!originalUrl || !source || !license) {
+    //   return 'https://data.utrechttimemachine.nl/sites/default/files/2025-05/Afbeelding-tijdelijk-niet-beschikbaar_3.png';
+    // }
     return originalUrl;
   }
 }
